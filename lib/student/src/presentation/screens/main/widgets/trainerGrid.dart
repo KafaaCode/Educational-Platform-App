73b908164187d6d5fa124e806fa6bffc3c633571
Omar Saiouf <homar0083@gmail.com>
2025-02-03 20:34:59 +0300

    add main page to route

--- lib/student/src/presentation/screens/main/widgets/trainerGrid.dart
+++ lib/student/src/presentation/screens/main/widgets/trainerGrid.dart
@@ -1,90 +1,75 @@
-
-import 'package:educational_platform_app/student/src/presentation/screens/login/widgets/Text_field_widget.dart';
-import 'package:educational_platform_app/student/src/presentation/widgets/app_scaffold.dart';
-import 'package:educational_platform_app/student/src/presentation/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-class TrainerGrid extends StatelessWidget {
 
-final String name;
-final String count_Student;
-final String path;
-final String spaczaltion;
-  TrainerGrid({required this.name,required this.count_Student,required this.path,required this.spaczaltion});
+class TrainerGrid extends StatelessWidget {
+  final String name;
+  final String count_Student;
+  final String path;
+  final String spaczaltion;
+  TrainerGrid(
+      {required this.name,
+      required this.count_Student,
+      required this.path,
+      required this.spaczaltion});
   @override
   Widget build(BuildContext context) {
-     
     return Wrap(
-  spacing: 2,
-  runSpacing: 5,
-  children: List.generate(6, (index) {
-    return Container(
-      width: MediaQuery.of(context).size.width > 800
-          ? MediaQuery.of(context).size.width * 0.23 
-          : MediaQuery.of(context).size.width > 550
-              ? MediaQuery.of(context).size.width * 0.3 
-              : MediaQuery.of(context).size.width * 0.47, 
-      child: ClipPath(
-        clipper: RoundedTopEdgeClipper(),
-        child: Card(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 35), 
-
-                ClipOval(
-                  child: Image.asset(
-                    '${this.path}',
-                    width: 60, 
-                    height: 60,
-                    fit: BoxFit.cover,
-                  ),
+      spacing: 2,
+      runSpacing: 5,
+      children: List.generate(6, (index) {
+        return Container(
+          width: MediaQuery.of(context).size.width > 800
+              ? MediaQuery.of(context).size.width * 0.23
+              : MediaQuery.of(context).size.width > 550
+                  ? MediaQuery.of(context).size.width * 0.3
+                  : MediaQuery.of(context).size.width * 0.47,
+          child: ClipPath(
+            clipper: RoundedTopEdgeClipper(),
+            child: Card(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 35),
+                    ClipOval(
+                      child: Image.asset(
+                        '${this.path}',
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(
+                      '${this.name}',
+                      style: const TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '${this.spaczaltion}',
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '${this.count_Student}طالب',
+                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-
-               
-
-                Text(
-                  '${this.name}',
-                  style: TextStyle(fontSize: 14),
-                  textAlign: TextAlign.center,
-                ),
-
-                Text(
-                  '${this.spaczaltion}',
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-
-                Text(
-                  '${this.count_Student}طالب',
-                  style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
-  }),
-);
-
   }
 }
- 
-
-
-
-
-
 
 class RoundedTopEdgeClipper extends CustomClipper<Path> {
   @override
@@ -92,27 +77,23 @@ class RoundedTopEdgeClipper extends CustomClipper<Path> {
     double radius = 50;
     Path path = Path();
 
-    
     path.moveTo(radius, 30);
-    path.lineTo(size.width / 2, 37); 
+    path.lineTo(size.width / 2, 37);
     path.lineTo(size.width - radius, 30);
-    
-    
+
     path.quadraticBezierTo(size.width, 30, size.width, 30 + radius);
     path.lineTo(size.width, size.height - radius);
 
-
-    path.quadraticBezierTo(size.width, size.height, size.width - radius, size.height);
+    path.quadraticBezierTo(
+        size.width, size.height, size.width - radius, size.height);
     path.lineTo(radius, size.height);
 
-    
     path.quadraticBezierTo(0, size.height, 0, size.height - radius);
     path.lineTo(0, 30 + radius);
 
-   
     path.quadraticBezierTo(0, 30, radius, 30);
 
-    path.close(); 
+    path.close();
     return path;
   }
 
--- lib/student/src/presentation/screens/main_ui/main_ui_student.dart
+++ lib/student/src/presentation/screens/main_ui/main_ui_student.dart
@@ -1,94 +1,132 @@
-import 'package:educational_platform_app/student/src/presentation/screens/login/widgets/Text_field_widget.dart';
 import 'package:educational_platform_app/student/src/presentation/screens/main/widgets/courseCard.dart';
 import 'package:educational_platform_app/student/src/presentation/screens/main/widgets/customAppBar.dart';
 import 'package:educational_platform_app/student/src/presentation/screens/main/widgets/sectionTitle.dart';
 import 'package:educational_platform_app/student/src/presentation/screens/main/widgets/trainerGrid.dart';
-import 'package:educational_platform_app/student/src/presentation/widgets/app_scaffold.dart';
-import 'package:educational_platform_app/student/src/presentation/widgets/button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 
 class Main_ui_scrren extends StatelessWidget {
+  const Main_ui_scrren({super.key});
+
   @override
   Widget build(BuildContext context) {
-   double screenWidth = MediaQuery.of(context).size.width;
-        bool isWideScreen = screenWidth >= 650;
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool isWideScreen = screenWidth >= 650;
     return Scaffold(
-      appBar: CustomAppBar(path:'assets/images/bg_intro_page1.png'),
-      body:Padding(
-       
-  padding: const EdgeInsets.all(8.0),
-  child: SingleChildScrollView(
-    child: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/bg_things.png"),
-          fit: BoxFit.cover, 
+        appBar: CustomAppBar(path: 'assets/images/bg_intro_page1.png'),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/bg_things.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  SectionTitle(title: 'الدورات الأكثر شيوعًا'),
+                  isWideScreen
+                      ? Row(
+                          children: [
+                            Expanded(
+                                child: CourseCard(
+                                    name: 'Ali sheakho',
+                                    time_triner: 10,
+                                    path: 'images/backgroundAppbar.png',
+                                    spaczaltion: 'التسويق الرقمي',
+                                    parce: 100,
+                                    evaluation: 5)),
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: CourseCard(
+                                    name: 'Ali sheakho',
+                                    time_triner: 10,
+                                    path: 'images/backgroundAppbar.png',
+                                    spaczaltion: 'التسويق الرقمي',
+                                    parce: 100,
+                                    evaluation: 5)),
+                          ],
+                        )
+                      : CourseCard(
+                          name: 'Ali sheakho',
+                          time_triner: 10,
+                          path: 'images/backgroundAppbar.png',
+                          spaczaltion: 'التسويق الرقمي',
+                          parce: 100,
+                          evaluation: 5),
+                  SectionTitle(title: 'نخبة المدربين'),
+                  TrainerGrid(
+                      name: 'Adel kharzom',
+                      count_Student: '1000',
+                      path: 'assets/images/bg_intro_page1.png',
+                      spaczaltion: 'التسويق الرقمي'),
+                  SectionTitle(title: 'العروض والمحتوى المميز'),
+                  isWideScreen
+                      ? Row(
+                          children: [
+                            Expanded(
+                                child: CourseCard(
+                                    name: 'Ali sheakho',
+                                    time_triner: 10,
+                                    path: 'images/backgroundAppbar.png',
+                                    spaczaltion: 'التسويق الرقمي',
+                                    parce: 100,
+                                    evaluation: 5)),
+                            const SizedBox(width: 10),
+                            Expanded(
+                                child: CourseCard(
+                                    name: 'Ali sheakho',
+                                    time_triner: 10,
+                                    path: 'images/backgroundAppbar.png',
+                                    spaczaltion: 'التسويق الرقمي',
+                                    parce: 100,
+                                    evaluation: 5)),
+                          ],
+                        )
+                      : CourseCard(
+                          name: 'Ali sheakho',
+                          time_triner: 10,
+                          path: 'images/backgroundAppbar.png',
+                          spaczaltion: 'التسويق الرقمي',
+                          parce: 100,
+                          evaluation: 5),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          
-          SizedBox(height: 20),
-          SectionTitle(title: 'الدورات الأكثر شيوعًا'),
-          isWideScreen ?  Row(
-            children: [
-              Expanded(child: CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5)),
-              SizedBox(width: 10),
-              Expanded(child: CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5)),
-            ],  )  :CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5),
-        
-           
-          SectionTitle(title: 'نخبة المدربين'),
-          TrainerGrid(name:'Adel kharzom',count_Student:'1000',path:'assets/images/bg_intro_page1.png',spaczaltion:'التسويق الرقمي'),
-          SectionTitle(title: 'العروض والمحتوى المميز'),
-            isWideScreen ?  Row(
-            children: [
-              Expanded(child: CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5)),
-              SizedBox(width: 10),
-              Expanded(child: CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5)),
-            ],  )  :CourseCard(name:'Ali sheakho',time_triner: 10,path:'images/backgroundAppbar.png',spaczaltion:'التسويق الرقمي',parce: 100,evaluation: 5),
-        ],
-      ),
-    ),
-  ),
-),
-bottomNavigationBar: ClipRRect(
-  borderRadius: BorderRadius.only(
-    topLeft: Radius.circular(20), 
-    topRight: Radius.circular(20), 
-  ),
-  child: BottomNavigationBar(
-    backgroundColor: Color(0xFF588760),
-    selectedItemColor: Colors.white,
-    unselectedItemColor: Colors.white70,
-    items: [
-      BottomNavigationBarItem(
-        backgroundColor: Color(0xFF588760),
-        icon: Icon(Icons.home),
-        label: 'الرئيسية',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.message),
-        label: 'الرسائل',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.book),
-        label: 'الدورات',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.person),
-        label: 'الملف الشخصي',
-      ),
-    ],
-  ),
-)
-
-    );
+        bottomNavigationBar: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: const Color(0xFF588760),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white70,
+            items: const [
+              BottomNavigationBarItem(
+                backgroundColor: Color(0xFF588760),
+                icon: Icon(Icons.home),
+                label: 'الرئيسية',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.message),
+                label: 'الرسائل',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'الدورات',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'الملف الشخصي',
+              ),
+            ],
+          ),
+        ));
   }
 }
-
