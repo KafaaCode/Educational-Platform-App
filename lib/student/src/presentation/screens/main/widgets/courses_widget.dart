ccbe79aafd973e112eca2646ee9b06158a08571e
Alisheakho <107799922+Alisheakho@users.noreply.github.com>
2025-02-05 11:05:29 +0300

    finished

--- lib/student/src/presentation/screens/main/widgets/courses_widget.dart
+++ lib/student/src/presentation/screens/main/widgets/courses_widget.dart
@@ -8,44 +8,50 @@ class CoursesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          
-                Text(
-            'جميع الدورات',
-            style: const TextStyle(
-              fontSize: 18,
-              fontFamily: 'Cairo',
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-       
-              Wrap(
-        spacing: 2,
-        runSpacing: 5,
-        children: List.generate(6, (index) {
-          return Container(
-             width: MediaQuery.of(context).size.width > 971
-             ?MediaQuery.of(context).size.width * 0.30
-              :MediaQuery.of(context).size.width > 800
-                ? MediaQuery.of(context).size.width * 0.47
-                : MediaQuery.of(context).size.width > 621
-                    ? MediaQuery.of(context).size.width * 0.47
-                    : MediaQuery.of(context).size.width * 0.99,
-                    child: CourseCard(
-                            index: 2,
-                            name: 'Ali sheakho',
-                            time_triner: 10,
-                            path: 'images/backgroundAppbar.png',
-                            spaczaltion: 'التسويق الرقمي',
-                            parce: 100,
-                            evaluation: 5),
-          );
-        }),
+        child: Padding(
+           padding: EdgeInsets.symmetric(
+    horizontal: MediaQuery.of(context).size.width * 0.02, // 5% من العرض
+
+  ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            SizedBox(height: 20),
+                  Text(
+              'جميع الدورات',
+              style: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'Cairo',
+                fontWeight: FontWeight.bold,
               ),
-         
-          ],
+            ),
+                 SizedBox(height: 5),   
+                Wrap(
+          spacing: 2,
+          runSpacing: 5,
+          children: List.generate(6, (index) {
+            return Container(
+               width: MediaQuery.of(context).size.width > 971
+               ?MediaQuery.of(context).size.width * 0.31
+                :MediaQuery.of(context).size.width > 800
+                  ? MediaQuery.of(context).size.width * 0.47
+                  : MediaQuery.of(context).size.width > 621
+                      ? MediaQuery.of(context).size.width * 0.47
+                      : MediaQuery.of(context).size.width * 0.99,
+                      child: CourseCard(
+                              index: 2,
+                              name: 'Ali sheakho',
+                              time_triner: 10,
+                              path: 'images/backgroundAppbar.png',
+                              spaczaltion: 'التسويق الرقمي',
+                              parce: 100,
+                              evaluation: 5),
+            );
+          }),
+                ),
+           
+            ],
+          ),
         ),
       ),
     );
